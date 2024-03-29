ee2993cc991ecb6dc7c8012022534ab3411ab3b1
JOHN-KWIZERA <j.kwizera@alustudent.com>
2024-03-29 13:51:13 +0200

    yes

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a url and displays its response body size
-curl -sI "$1" | grep "Content-Length" | awk '{print $2}'
+# getting content and displaying just content lenght information 
+curl -sI "$1" | grep "Content-Length" | cut -d' ' -f 2
